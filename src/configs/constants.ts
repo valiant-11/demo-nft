e1989b558a3770c34e98e42d235ccfe089114b2b
junmarkbrilliantes1 <junmarkbrilliantes1@gmail.com>
2025-11-09 01:35:51 +0000

    performative type shi

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,3 +1,3 @@
-export const CONTRACTPACKAGEID = "";
-export const CONTRACTMODULENAME = "";
-export const CONTRACTMODULEMETHOD = "";
+export const CONTRACTPACKAGEID = "0x31647f7d9c641dc3106c700e63d1ac36ab7e3618439bb23fc05cc88a75b92514";
+export const CONTRACTMODULENAME = "nft_marketplace";
+export const CONTRACTMODULEMETHOD = "mint_to_sender";
